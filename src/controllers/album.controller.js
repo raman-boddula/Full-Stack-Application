dc0c83359bdb34704c1c834c54caeaca69100359
Raman Boddula <raman_rc001@yahoo.com>
2022-02-18 23:59:09 +0530

    fixed

--- src/controllers/album.controller.js
+++ src/controllers/album.controller.js
@@ -1,24 +1,23 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
 const Album = require("../models/album.model");
 
 router.post("/", async (req, res) => {
-    try {
-        const album = await Album.create(req.body);
-        res.status(200).json({album:album})
-     }
-    catch (err) {
-        res.status(500).json({ Staus: "failed", error: e.message });
-    }
-})
+  try {
+    const album = await Album.create(req.body);
+    res.status(200).json({ album: album });
+  } catch (err) {
+    res.status(500).json({ Status: "failed", error: e.message });
+  }
+});
 router.get("/", async (req, res) => {
   try {
     const albums = await Album.find().lean().exec();
     res.status(200).json({ albums: albums });
   } catch (err) {
-    res.status(500).json({ Staus: "failed", error: e.message });
+    res.status(500).json({ Status: "failed", error: e.message });
   }
 });
 router.get("/:id", async (req, res) => {
@@ -26,21 +25,19 @@ router.get("/:id", async (req, res) => {
     const album = await Album.findById(req.params.id).lean().exec();
     res.status(200).json({ album: album });
   } catch (err) {
-    res.status(500).json({ Staus: "failed", error: e.message });
+    res.status(500).json({ Status: "failed", error: e.message });
   }
 });
 router.patch("/:id", async (req, res) => {
   try {
-    const album = await Album.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true }
-    )
+    const album = await Album.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    })
       .lean()
       .exec();
     res.status(200).json({ album: album });
   } catch (err) {
-    res.status(500).json({ Staus: "failed", error: e.message });
+    res.status(500).json({ Status: "failed", error: e.message });
   }
 });
 router.delete("/:id", async (req, res) => {
@@ -48,7 +45,7 @@ router.delete("/:id", async (req, res) => {
     const album = await Album.findByIdAndDelete(req.params.id).lean().exec();
     res.status(200).json({ album: album });
   } catch (err) {
-    res.status(500).json({ Staus: "failed", error: e.message });
+    res.status(500).json({ Status: "failed", error: e.message });
   }
 });
 module.exports = router;
--- src/controllers/songs.controller.js
+++ src/controllers/songs.controller.js
@@ -6,56 +6,56 @@ const Songs = require("../models/songs.model");
 
 router.post("/", async (req, res) => {
   try {
-    const album = await Songs.create(req.body);
-    res.status(200).json({ album: album });
+    const songs = await Songs.create(req.body);
+    res.status(200).json({ songs: songs });
   } catch (err) {
-    res.status(500).json({ Staus: "failed", error: e.message });
+    res.status(500).json({ Status: "failed", error: e.message });
   }
 });
 router.get("/", async (req, res) => {
   try {
-    const albums = await Songs.find()
+    const songs = await Songs.find()
       .populate("album_id")
       .populate("singer_id")
       .lean()
       .exec();
-    res.status(200).json({ albums: albums });
+    res.status(200).json({ songs: songs });
   } catch (err) {
-    res.status(500).json({ Staus: "failed", error: e.message });
+    res.status(500).json({ Status: "failed", error: e.message });
   }
 });
 router.get("/:id", async (req, res) => {
   try {
-    const album = await Songs.findById(req.params.id)
+    const songs = await Songs.findById(req.params.id)
       .populate("album_id")
       .populate("singer_id")
       .lean()
       .exec();
-    res.status(200).json({ album: album });
+    res.status(200).json({ songs: songs });
   } catch (err) {
-    res.status(500).json({ Staus: "failed", error: e.message });
+    res.status(500).json({ Status: "failed", error: e.message });
   }
 });
 router.patch("/:id", async (req, res) => {
   try {
-    const album = await Songs.findByIdAndUpdate(req.params.id, req.body, {
+    const songs = await Songs.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
     })
       .populate("album_id")
       .populate("singer_id")
       .lean()
       .exec();
-    res.status(200).json({ album: album });
+    res.status(200).json({ songs: songs });
   } catch (err) {
-    res.status(500).json({ Staus: "failed", error: e.message });
+    res.status(500).json({ Status: "failed", error: e.message });
   }
 });
 router.delete("/:id", async (req, res) => {
   try {
-    const album = await Songs.findByIdAndDelete(req.params.id).lean().exec();
-    res.status(200).json({ album: album });
+    const songs = await Songs.findByIdAndDelete(req.params.id).lean().exec();
+    res.status(200).json({ songs: songs });
   } catch (err) {
-    res.status(500).json({ Staus: "failed", error: e.message });
+    res.status(500).json({ Status: "failed", error: e.message });
   }
 });
 module.exports = router;
